2566b9a31d17837fddfdbf3a01201f040d9c18a5
IzzyLerman <izzylerman14@gmail.com>
2024-11-29 17:02:37 -0800

    public upload

---
+++ cypress/support/commands.js
@@ -0,0 +1,31 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+require("@testing-library/cypress/add-commands")
+
+Cypress.Commands.add("blank", function (text) {
+    // cy.findByPlaceholderText("Enter a To-Do").type(text)
+    // cy.findByRole("button", { name: "Add To-Do" }).click()
+})
